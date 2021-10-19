d96d8f35e6021debf164407990e7ca732ac4d245
Matt K Lawrence <88916442+MattKLawrence@users.noreply.github.com>
2021-10-19 22:18:15 +0200

    Add files via upload

---
+++ DeVolutions-Logo-OnScreen-FiveM/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version "cerulean"
+games {"rdr3", "gta5"}
+author "Matt K Lawrence"
+company "DeVolutions"
+description "Logo Spin x FiveM"
+version "1.0.0"
+
+files {
+    "index.html",
+    "YOUR-LOGO.gif"
+}
+
+client_script {
+    "client.lua"
+}
+
+ui_page {
+    "index.html",
+}
